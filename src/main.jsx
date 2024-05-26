00cb717570dd72af4551148f20173dc05891fa92
Fayez Rahman <fayezrahman@Fayezs-MacBook-Pro.local>
2024-05-26 16:03:33 +0100

    add chakra ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
 );
