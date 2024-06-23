42b4045da6f8749d82ad53711913b4784cee00c5
Fayez Rahman <fayezrahman@Fayezs-MacBook-Pro.local>
2024-06-23 15:42:03 +0100

    add functionality to make api call to update checkin

--- src/components/EditCheckIn.jsx
+++ src/components/EditCheckIn.jsx
@@ -1,16 +1,42 @@
+import { Button, Flex, useToast } from '@chakra-ui/react';
 import { useAdminQuestion } from '../context/AdminProvider';
 import QuestionsSummary from './QuestionsSummary';
+import { client } from '../util/axios-util';
 const EditCheckIn = ({ checkInId }) => {
   const { submittedCheckIns, setSubmittedCheckIns } = useAdminQuestion();
-  console.log('test', checkInId);
-  console.log(submittedCheckIns);
+  const toast = useToast();
   const checkInToEdit = submittedCheckIns.find(
     (checkIn) => checkIn.checkInId === checkInId
   );
-  console.log('check in to edit', checkInToEdit);
+
+  async function handleSaveCheckIn() {
+    console.log('the checkIn that is updated', checkInToEdit);
+    console.log('call api to save checkIn');
+    try {
+      const response = await client.post('/admin/updateCheckIn', checkInToEdit);
+      toast({
+        title: response.data.message,
+        status: 'success',
+        duration: 3000,
+        isClosable: true
+      });
+
+    } catch (error) {
+      toast({
+        title: 'Failed to Update Check-in',
+        description: error.response?.data?.message || 'An error occurred',
+        status: 'error',
+        duration: 3000,
+        isClosable: true
+      });
+    }
+  }
 
   return (
     <>
+      <Flex justifyContent='flex-end'>
+        <Button colorScheme='orange' size='sm' onClick={handleSaveCheckIn}>Save Check-in</Button>
+      </Flex>
       <QuestionsSummary
         checkIn={checkInToEdit}
         setCheckIn={setSubmittedCheckIns}
