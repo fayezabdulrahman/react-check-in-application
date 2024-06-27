aa21be934fa70a043c1be97640737ee651dbfc33
Fayez Rahman <fayezrahman@Fayezs-MacBook-Pro.local>
2024-06-27 19:00:22 +0100

    add functionality to delete and unpublish checkin

--- src/components/admin/AvailableCheckIn.jsx
+++ src/components/admin/AvailableCheckIn.jsx
@@ -10,6 +10,7 @@ import {
   Text,
   useToast
 } from '@chakra-ui/react';
+import { MdDeleteOutline } from 'react-icons/md';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { useAdmin } from '../../context/AdminProvider';
 import { client } from '../../util/axios-util';
@@ -25,6 +26,8 @@ const AvailableCheckIn = () => {
   } = useAdmin();
   const [loading, setLoading] = useState(true);
   const [publishing, setPublishing] = useState(false);
+  const [deleting, setDeleting] = useState(false);
+  const [unpublishing, setUnpublishing] = useState(false);
 
   const toast = useToast();
 
@@ -47,7 +50,7 @@ const AvailableCheckIn = () => {
     };
     fetchCreatedCheckins();
     // added publishedCheckIn as dependecny because when we publish a check-in we want to re-render so we can disable publish button
-  }, [setSubmittedCheckIns, publishedCheckIn]);
+  }, [setSubmittedCheckIns, publishedCheckIn, deleting]);
 
   async function handlePublishCheckIn(checkInId) {
     setPublishing(true);
@@ -80,6 +83,68 @@ const AvailableCheckIn = () => {
     }
   }
 
+  async function handleDeleteCheckIn(checkInId) {
+    setDeleting(true);
+
+    console.log('checkIn to delet', checkInId);
+
+    const payload = {
+      checkInToDelete: checkInId
+    };
+    try {
+      const response = await client.post('/admin/deleteCheckIn', payload);
+      console.log('response from server', response.data);
+      const message = response.data.message;
+
+      toast({
+        title: message,
+        status: 'success',
+        duration: 3000,
+        isClosable: true
+      });
+    } catch (error) {
+      console.log('error');
+      toast({
+        title: error.response?.data?.message || 'An error occurred',
+        status: 'error',
+        duration: 3000,
+        isClosable: true
+      });
+    } finally {
+      setDeleting(false);
+    }
+  }
+
+  async function handleUnPublishCheckIn(checkInId) {
+    setUnpublishing(true);
+    const payload = {
+      checkInToUnpublish: checkInId
+    };
+    try {
+      const response = await client.post('/admin/unPublishCheckIn', payload);
+      const message = response.data.message;
+      const serverPublishedCheckIn = response.data.checkIn;
+
+      setPublishedCheckIn(serverPublishedCheckIn);
+      toast({
+        title: message,
+        status: 'success',
+        duration: 3000,
+        isClosable: true
+      });
+    } catch (error) {
+      console.log('error');
+      toast({
+        title: error.response?.data?.message || 'An error occurred',
+        status: 'error',
+        duration: 3000,
+        isClosable: true
+      });
+    } finally {
+      setUnpublishing(false);
+    }
+  }
+
   if (loading) {
     return <Loading />;
   }
@@ -95,7 +160,7 @@ const AvailableCheckIn = () => {
             <Text>Published: {availableCheckIn.published ? 'Yes' : 'No'} </Text>
             <Text>Questions length: {availableCheckIn.questions.length}</Text>
           </CardBody>
-          <CardFooter>
+          <CardFooter justifyContent="space-between">
             <ButtonGroup>
               <Button
                 isLoading={publishing}
@@ -107,16 +172,34 @@ const AvailableCheckIn = () => {
               >
                 Publish
               </Button>
+              {availableCheckIn.published && (
+                <Button
+                  variant="solid"
+                  colorScheme="orange"
+                  onClick={() => handleUnPublishCheckIn(availableCheckIn.checkInId)}
+                >
+                  Unpublish
+                </Button>
+              )}
+
               <Button
                 as={ReactRouterLink}
-                to='/admin/editCheckIn'
-                state={{checkInId: availableCheckIn.checkInId}}
+                to="/admin/editCheckIn"
+                state={{ checkInId: availableCheckIn.checkInId }}
                 variant="ghost"
                 colorScheme="orange"
               >
                 Edit
               </Button>
             </ButtonGroup>
+            <Button
+              isLoading={unpublishing}
+              loadingText="Unpublishing..."
+              leftIcon={<MdDeleteOutline />}
+              onClick={() => handleDeleteCheckIn(availableCheckIn.checkInId)}
+            >
+              Delete
+            </Button>
           </CardFooter>
         </Card>
       ))}
