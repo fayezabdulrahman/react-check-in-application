549f1e347a0ea19b84e50365817f462c061e40da
Fayez Abdulrahman <fayez.rahman@hotmail.com>
2025-02-01 15:13:33 +0000

    fix backend api endpoint

--- src/util/axios-util.js
+++ src/util/axios-util.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const client = axios.create({
-  baseURL: 'http://localhost:9000',
+  baseURL: 'http://localhost:9000/api',
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true
