a51ab2ee27c807af1a052334f85c73aff47921d7
Fayez Abdulrahman <fayez.rahman@hotmail.com>
2025-02-08 15:21:06 +0000

    Tidy up registration page layout and styles and add background colour

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -12,8 +12,9 @@ import {
 import Login from '../components/Login';
 import Signup from '../components/Signup';
 import { useState } from 'react';
+import backgroundLogo from '../assets/b2b_second.jpg';
 const Registration = () => {
-  const [tabIndex, setTabIndex]= useState(0);
+  const [tabIndex, setTabIndex] = useState(0);
 
   const handleTabChange = (index) => {
     setTabIndex(index);
@@ -25,37 +26,49 @@ const Registration = () => {
 
   return (
     <>
-      <Container maxW="2xl" centerContent p={6} bg="black">
-        <Box
-          display="flex"
-          justifyContent="center"
-          p={3}
-          bg={'white'}
-          w="100%"
-          m="40px 0 15px 0"
-          borderRadius="lg"
-          borderWidth="2px"
-        >
-          <Text fontSize="2xl">Check Me In Application</Text>
-        </Box>
+      <Box
+        bgImage={`url(${backgroundLogo})`}
+        bgSize="cover"
+        bgPosition="center"
+        height="100vh"
+        padding={'5rem'}
+      >
+        <Container maxW="2xl" centerContent p={6} bg={'white'} >
+          <Box
+            display="flex"
+            justifyContent="center"
+            p={3}
+            w="100%"
+            m="40px 0 15px 0"
+            borderRadius="lg"
+            borderWidth="2px"
+          >
+            <Text fontSize="2xl">Ez Check-ins</Text>
+          </Box>
 
-        <Box bg="white" p={4} borderRadius="lg" borderWidth="2px" w="100%">
-          <Tabs variant="soft-rounded" colorScheme="orange" index={tabIndex} onChange={handleTabChange}>
-            <TabList mb="1rem">
-              <Tab w="50%">Login</Tab>
-              <Tab w="50%">Sign Up</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-                <Login />
-              </TabPanel>
-              <TabPanel>
-                <Signup switchToLoginTab={switchToLoginTab} />
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </Box>
-      </Container>
+          <Box p={4} borderRadius="lg" borderWidth="2px" w="100%">
+            <Tabs
+              variant="soft-rounded"
+              colorScheme="orange"
+              index={tabIndex}
+              onChange={handleTabChange}
+            >
+              <TabList mb="1rem">
+                <Tab w="50%">Login</Tab>
+                <Tab w="50%">Sign Up</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <Login />
+                </TabPanel>
+                <TabPanel>
+                  <Signup switchToLoginTab={switchToLoginTab} />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+        </Container>
+      </Box>
     </>
   );
 };
