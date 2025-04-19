8158b30d0ec7ca880850c3aadf3cd13bbc1664d3
Fayez Abdulrahman <fayez.rahman@hotmail.com>
2025-04-19 18:40:57 +0100

    Add footer component

--- src/Pages/ProtectedRoute.jsx
+++ src/Pages/ProtectedRoute.jsx
@@ -3,6 +3,7 @@ import Navigation from '../components/Navigation';
 // import { useAuth } from '../context/AuthProvider';
 import { useAuth0 } from '@auth0/auth0-react';
 import Loading from '../components/shared/Loading';
+import Footer from './Footer';
 const ProtectedRoute = ({ allowedRoles }) => {
   // const { token, userState } = useAuth();
   const { isAuthenticated, user, isLoading } = useAuth0();
@@ -26,6 +27,7 @@ const ProtectedRoute = ({ allowedRoles }) => {
     <>
       <Navigation />
       <Outlet />
+      <Footer />
     </>
   );
 };
