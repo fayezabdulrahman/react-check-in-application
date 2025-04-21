baf6061891e25c33e96c1d6bd8ada6ad253891ac
Fayez Abdulrahman <fayez.rahman@hotmail.com>
2025-04-22 00:39:16 +0100

    Code tidy up for refresh bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Landing from './Pages/Landing';
 import LocalAuthProvider from './context/LocalAuthProvider';
 import AdminHomepage from './components/admin/AdminHomepage';
 import DetailedPublishedCheckIn from './components/admin/DetailedPublishedCheckIn';
-import { Auth0Provider } from '@auth0/auth0-react';
 import AvailableCheckIn from './components/admin/AvailableCheckIn';
 import ProtectedComponents from './components/shared/ProtectedComponents';
 import SubmittedCheckIn from './components/user/SubmittedCheckIn';
@@ -16,16 +15,10 @@ import { useState, useEffect } from 'react';
 import Loading from './components/shared/Loading';
 
 function App() {
-  const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const auth0ClientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const auth0Audience = import.meta.env.VITE_AUTH0_AUDIENCE;
-
-  const { isLoading, error} = useAuth0(); // Add this
+  const { isLoading, error} = useAuth0();
   const [appLoading, setAppLoading] = useState(true);
 
   useEffect(() => {
-    console.log('is loading auth 0 ', isLoading);
-    console.log('is loading auth 0 ', error);
     if (!isLoading || error) {
       setAppLoading(false);
     }
@@ -41,17 +34,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Auth0Provider
-          domain={auth0Domain}
-          clientId={auth0ClientId}
-          authorizationParams={{
-            redirect_uri: window.location.origin,
-            audience: auth0Audience
-          }}
-          cacheLocation="localstorage"
-          useRefreshTokens={true}
-          onError={(error) => console.error('Auth0 Error:', error)}
-        > */}
           <LocalAuthProvider>
             <Routes>
               <Route path="/" element={<Landing />} />
@@ -85,7 +67,6 @@ function App() {
             </Routes>
             <ProtectedComponents />
           </LocalAuthProvider>
-        {/* </Auth0Provider> */}
       </BrowserRouter>
     </>
   );
