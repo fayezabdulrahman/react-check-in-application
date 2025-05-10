76dd97c90194483031ea7afd075b2133af301baa
Fayez Abdulrahman <fayez.rahman@hotmail.com>
2025-05-10 21:03:08 +0100

    Add new UI to show duplcate menu for each created check in card

--- src/components/checkinBuilder/CheckInSubmitter.jsx
+++ src/components/checkinBuilder/CheckInSubmitter.jsx
@@ -11,43 +11,24 @@ import {
   Icon,
   Box
 } from '@chakra-ui/react';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import useAdminService from '../../hooks/services/useAdminService';
 import { useRef, useState } from 'react';
 import { useLocalAuth } from '../../context/LocalAuthProvider';
 import { MdPublish, MdHelpOutline } from 'react-icons/md';
 import useCheckInStore from '../../store/checkin-store';
+import { useCreateCheckIn } from '../../hooks/useCreateCheckIn';
 
 export const CheckInSubmitter = () => {
   const questions = useCheckInStore((state) => state.questions);
   const resetQuestions = useCheckInStore((state) => state.resetQuestions);
 
   const { userDetails } = useLocalAuth();
-  const { createAdminCheckIn } = useAdminService();
-  const queryClient = useQueryClient();
   const toast = useToast();
   const checkInNameRef = useRef();
   const [isCheckInAnonymous, setIsCheckInAnonymous] = useState(false);
 
-  const { mutate: saveCheckIn, isPending } = useMutation({
-    mutationFn: createAdminCheckIn,
-    onSuccess: () => {
-      toast({
-        title: 'Check-in Created!',
-        status: 'success',
-        duration: 3000
-      });
-      queryClient.refetchQueries(['allAdminCheckIn']);
-      resetQuestions();
-    },
-    onError: (error) => {
-      toast({
-        title: 'Error Creating Check-in',
-        description: error.response?.data?.message,
-        status: 'error',
-        duration: 3000
-      });
-    }
+  const { mutate: saveCheckIn, isPending} = useCreateCheckIn({
+    refetchQueryKeys: [['allAdminCheckIn']],
+    afterSuccess: resetQuestions
   });
 
   const handleSaveCheckIn = () => {
